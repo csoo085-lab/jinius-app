00287d914c73aace2da84d79dd12a9819a206d55
csoo085-lab <csoo085@naver.com>
2026-09-20 06:43:56 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
